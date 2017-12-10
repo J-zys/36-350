a69cb9eba0b18d49edfd5f9dffda64bbcbd04ab9
J-zys <jessonchou@gmail.com>
2017-12-09 19:30:25 -0500

    Add files via upload

---
+++ hello_aliens.R
@@ -0,0 +1,12 @@
+message = "
+Hello Aliens, 
+I hope this message finds you well. This is from Earth, 
+the 3rd planet from the sun at a distance of 150 million kilometers. 
+Earth is a great planet! Please come visit us and find out more about us. 
+What are you guys like? 
+We are eager to meet you! 
+
+Best,
+Jason Zhou
+"
+cat(message)
